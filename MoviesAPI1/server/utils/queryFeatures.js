046f99e9df948877f3375c54ce3d42298d0e97cc
Amadou-Marong <marongamadou6@gmail.com>
2025-02-10 10:30:44 +0000

    query features utility function

--- MoviesAPI1/server/utils/queryFeatures.js
+++ MoviesAPI1/server/utils/queryFeatures.js
@@ -1,4 +1,7 @@
 // query feautures component
 export const queryFeatures = (model, queryParams) => {
+    // Destructure the query parameters
     const {sort, fields, page = 1, limit = 10, ...filters} = queryParams
+
+
 }
