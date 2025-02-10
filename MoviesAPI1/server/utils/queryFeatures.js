26edd0ceb60027ab94f177a6bddddabdf4924970
Amadou-Marong <marongamadou6@gmail.com>
2025-02-10 10:37:30 +0000

    query features utility function

--- MoviesAPI1/server/utils/queryFeatures.js
+++ MoviesAPI1/server/utils/queryFeatures.js
@@ -1,7 +1,20 @@
 // query feautures component
 export const queryFeatures = (model, queryParams) => {
     // Destructure the query parameters
-    const {sort, fields, page = 1, limit = 10, ...filters} = queryParams
+    let {sort, fields, page = 1, limit = 10, ...filters} = queryParams
 
+    // Convert query parameters to mongoDB query
+    let queryStr = JSON.stringify(filters).replace(
+        /\b(gt|gte|lt|lte)\b/g,
+        match => `$${match}`
+    )
+
+    filters = JSON.parse(queryStr)
+
+    // create mongoDB query
+    let query = model.find(filters)
+
+    
+   
 
 }
