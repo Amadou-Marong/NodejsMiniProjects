0aed72b75c28984d3efcf9d53674e1b220ab62ae
Amadou-Marong <marongamadou6@gmail.com>
2025-02-13 09:54:58 +0000

    role based auth login function

--- RolesAuth/server/controllers/authController.js
+++ RolesAuth/server/controllers/authController.js
@@ -1,23 +1,55 @@
 import { User } from "../models/userModel.js";
-
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const registerUser = async (req, res) => {
     try {
-        const user = await User.create(req.body)
+        const {name, email, password, role} = req.body
+        const user = await User.findOne({email: email})
+        if (user) {
+            return res.status(400).send({
+                message: "User with this email already exists"
+            })
+        }
+        const salt = await bcrypt.genSalt(10)
+        let hasPassword = await bcrypt.hash(password, salt)
+        
+        let userData = new User({
+            name,
+            email,
+            password: hasPassword,
+            role
+        })
+
+        userData.save()
+
         return res.status(201).send({
-            message: "User created successfully",
-            data: user
+            message: "User Registered Successfully",
+            data: userData
         })
     } catch (error) {
         return res.status(500).send({
-            message: "Failed to create user",
+            message: "Failed to Register user",
             data: error
         })
     }
 }
 
 export const loginUser = async (req, res) => {
-    return res.status(200).send("Login User")
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email: email})
+        const match = await bcrypt.compare(password, user.password)
+
+        if(!user || !match) {
+            return res.status(200).send({
+                message: "Invalid Email or Password"
+            })
+        }
+        
+    } catch (error) {
+        
+    }
 }
 
 export const logoutUser = async (req, res) => {
