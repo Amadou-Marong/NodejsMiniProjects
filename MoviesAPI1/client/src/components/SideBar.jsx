2da28f3f63efe0f96486383e61320611c08ddc63
Amadou-Marong <marongamadou6@gmail.com>
2025-02-14 22:03:44 +0000

    default global error handling

--- MoviesAPI1/client/src/components/SideBar.jsx
+++ MoviesAPI1/client/src/components/SideBar.jsx
@@ -1,7 +1,26 @@
+import { Home, CalendarSearch, UserPlus, Shield, Icon } from "lucide-react"
 const SideBar = () => {
+  const menuItems = [
+    { icon: Home, label: 'Employees', path: '/employees' },
+    // leave balances
+    { icon: CalendarSearch, label: 'Leave Balances', path: '/leave-balances' },
+    { icon: UserPlus, label: 'Register Employee', path: '/register' },
+    { icon: Shield, label: 'Roles & Permissions', path: '/roles' }
+  ]
   return (
-    <div className="h-screen top-0 left-0 absolute border-r w-40">
-        
+    <div className="h-screen bg-gray-700 text-white fixed left-0 top-17 transition-all duration-300 w-56">
+        <div className="p-4">
+          <nav className="space-y-1">
+            {menuItems.map((item, key) => {
+                const Icon = item.icon
+                return (
+                   <a key={key} href="" className="flex items-center gap-2 space-y-2">
+                      <Icon size={20} /> <span className="text-sm">{item.label}</span>
+                   </a>
+                )
+            })}
+          </nav>
+        </div>
     </div>
   )
 }
