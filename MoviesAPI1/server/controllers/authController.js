ee2e0129570ffd5aa868ed35836e3554179a5345
Amadou-Marong <marongamadou6@gmail.com>
2025-02-16 18:37:54 +0000

    user controller

---
+++ MoviesAPI1/server/controllers/authController.js
@@ -0,0 +1,19 @@
+import express from "express"
+import { User } from "../models/userModel"
+
+export const signUp = async (req, res) => {
+    try {
+        const newUser = req.body
+        const user = await User.create(newUser)
+        
+        return res.status(201).send({
+            status: "success",
+            data: user
+        })
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: error
+        })
+    }
+}
