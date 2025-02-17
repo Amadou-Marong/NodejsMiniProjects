5b08405ac580ad9b0dbbf123b1639e60368f5181
Amadou-Marong <marongamadou6@gmail.com>
2025-02-17 04:22:09 +0000

    working on the login function

--- MoviesAPI1/server/controllers/authController.js
+++ MoviesAPI1/server/controllers/authController.js
@@ -1,13 +1,22 @@
 import express from "express"
 import { User } from "../models/userModel.js"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
 
 export const signUp = async (req, res) => {
     try {
         const newUser = req.body
         const user = await User.create(newUser)
         
+        const token = jwt.sign({
+            id: user._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
         return res.status(201).send({
             status: "success",
+            token,
             data: user
         })
     } catch (error) {
@@ -25,14 +34,23 @@ export const signIn = async (req, res) => {
 
         if (user) {
             const match = await bcrypt.compare(password, user.password)
-            
             if (match) {
+                const token = jwt.sign({
+                    id: user._id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: "1d"
+                })
                 return res.status(200).send({
                     status: "success",
+                    token,
                     data: user
                 })
             }
         }
+        return res.status(400).send({
+            status: "error",
+            message: "Invalid email or password"
+        })
     }catch(error){
         return res.status(500).send({
             status: "error",
